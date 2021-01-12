3800f5413dbc5e5c4e73ae0851e708e23f4e238b
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2021-01-12 21:54:00 +0200

    color

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -2,13 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Statistics.module.css";
 
+function getRandColor() {
+  let color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+  while (color.length < 6) {
+    color = "0" + color;
+  }
+  return "#" + color;
+}
+
 const Statistics = ({ title, stats }) => {
   return (
     <div className={styles.statistics}>
       {title && <h2 className={styles.title}>{title}</h2>}
       <ul className={styles.list}>
         {stats.map((stat) => (
-          <li className={styles.listItem} key={stat.id}>
+          <li
+            className={styles.listItem}
+            key={stat.id}
+            style={{ backgroundColor: getRandColor() }}
+          >
             <span className={styles.label}>{stat.label}</span>
             <span className={styles.percentage}>{stat.percentage}%</span>
           </li>
